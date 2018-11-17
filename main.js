9eb1327cc4cfccd51dc227dc6f977e99cc07867a
alisonberkowitz <alison.berkowitz@students.olin.edu>
2018-11-17 12:08:16 -0500

    Integrate Google Maps API

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-alert("this is dummy js");
+var map;
+function initMap() {
+	var mapOptions = {
+	    zoom: 10,
+	    center: new google.maps.LatLng(42.3581, -71.0636)
+	};
+	map = new google.maps.Map(document.getElementById('map-canvas'),
+		mapOptions);
+}
