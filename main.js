e791aab5bc8e06710617eb55c21ceac1bedbb20f
alisonberkowitz <alison.berkowitz@students.olin.edu>
2018-11-18 16:05:42 -0500

    Map instagram takeout dump

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var map;
-var markers;
 function initMap() {
 	var mapOptions = {
 	    zoom: 3,
@@ -7,10 +6,20 @@ function initMap() {
 	};
 	map = new google.maps.Map(document.getElementById('map-canvas'),
 		mapOptions);
-	markers = [];
 
 	for (var i = photos.data.length - 1; i >= 0; i--) {
-		addToMap(photos.data[i]);
+		addToMap(createModelFromPhoto(photos.data[i]));
+	};
+
+	mapInstaDownloads();
+}
+
+function createModelFromPhoto(photo) {
+	return {
+		caption: photo.caption ? photo.caption.text : null, 
+		taken_at: new Date(Number(photo["created_time"])*1000).toLocaleDateString(),
+		path: photo.images.standard_resolution.url,
+		location: photo.location
 	};
 }
 
@@ -25,7 +34,6 @@ function addToMap(photo) {
 		        title: photo.location.name,
 		        icon: '/pin.svg'
 		    });
-	markers.push(marker);
 	google.maps.event.addListener(marker, 'click', function() {
     	infowindow.open(map, marker);
   	});
@@ -33,11 +41,31 @@ function addToMap(photo) {
 
 function photoView(photo) {
     var html = '<div class="info-window-content">'
-    	+ '<img src=' + photo.images.standard_resolution.url + '>'
-        + '<p><b>' + new Date(Number(photo["created_time"])*1000).toLocaleDateString() + ':</b> ' + photo.location.name + '</p>';
+    	+ '<img src=' + photo.path + '>'
+        + '<p><b>' + photo.taken_at + ':</b> ' + photo.location.name + '</p>';
     if (photo.caption) {
-    	html += '<p>' + photo.caption.text + '</p>';
+    	html += '<p>' + photo.caption + '</p>';
     }
     html += '</div>'
     return html;
+}
+
+function mapInstaDownloads() {
+	for (var i = media.photos.length - 1; i >= 0; i--) {
+		addToMap(createModelFromInsta(media.photos[i]));
+	};
+}
+
+function createModelFromInsta(photo) {
+	photo.taken_at = new Date(photo.taken_at).toLocaleDateString();
+	photo.location = getLocation(photo.location);
+	return photo;
+}
+
+function getLocation(name) {
+	return {
+		name: name,
+		latitude: 41,
+		longitude: 71
+	};
 }
